f76be01180e6ad04bdfb751c0c1c081adb9d2ebf
pbl.gllgs <pbl.gllgs@gmail.com>
2021-09-22 16:27:19 -0300

    Initial commit pet-clinic course spring boot guru

---
+++ src/main/java/com/pblgllgs/sbpetclinic/SbpetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.pblgllgs.sbpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbpetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbpetclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pblgllgs/sbpetclinic/SbpetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pblgllgs.sbpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbpetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
