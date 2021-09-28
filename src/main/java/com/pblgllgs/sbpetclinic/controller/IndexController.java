963594bc197b2ddd9c5e922316b5b6e108489c53
pbl.gllgs <pbl.gllgs@gmail.com>
2021-09-28 16:03:26 -0300

    Update IndexController.java

--- src/main/java/com/pblgllgs/sbpetclinic/controller/IndexController.java
+++ src/main/java/com/pblgllgs/sbpetclinic/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping({"/","index","index.html"})
+    @RequestMapping({"/","/index","/index.html"})
     public String index(Model model){
         return "index";
     }
