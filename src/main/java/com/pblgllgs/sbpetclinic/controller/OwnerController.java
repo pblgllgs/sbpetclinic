f7de5bb1377c900fe88f564220efb68c29b474d6
pbl.gllgs <pbl.gllgs@gmail.com>
2021-09-28 17:33:15 -0300

    impl method

--- src/main/java/com/pblgllgs/sbpetclinic/controller/OwnerController.java
+++ src/main/java/com/pblgllgs/sbpetclinic/controller/OwnerController.java
@@ -1,5 +1,6 @@
 package com.pblgllgs.sbpetclinic.controller;
 
+import com.pblgllgs.sbpetclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
     public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
     return "owners/index";
     }
 }
--- src/main/java/com/pblgllgs/sbpetclinic/controller/VetController.java
+++ src/main/java/com/pblgllgs/sbpetclinic/controller/VetController.java
@@ -1,5 +1,6 @@
 package com.pblgllgs.sbpetclinic.controller;
 
+import com.pblgllgs.sbpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"","/","/index","/index.html"})
     public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
