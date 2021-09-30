56e1a7b0a07888943468b68059e053ebe41d1c34
pbl.gllgs <pbl.gllgs@gmail.com>
2021-09-30 18:23:47 -0300

    Create PetType service impl. Closes #13.

---
+++ src/main/java/com/pblgllgs/sbpetclinic/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.pblgllgs.sbpetclinic.service;
+
+import com.pblgllgs.sbpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long>{
+}
---
+++ src/main/java/com/pblgllgs/sbpetclinic/service/map/PetTypeServiceMap.java
@@ -0,0 +1,35 @@
+package com.pblgllgs.sbpetclinic.service.map;
+
+import com.pblgllgs.sbpetclinic.model.PetType;
+import com.pblgllgs.sbpetclinic.service.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType,Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
