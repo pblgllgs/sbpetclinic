ca4aacf6c7d502423b207bd6f3ea60617b4f5eab
pbl.gllgs <pbl.gllgs@gmail.com>
2021-10-01 15:46:43 -0300

    Create MapService and Impl Speciality. Closes #15.

---
+++ src/main/java/com/pblgllgs/sbpetclinic/service/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.pblgllgs.sbpetclinic.service;
+
+import com.pblgllgs.sbpetclinic.model.Specialty;
+
+public interface SpecialityService extends CrudService<Specialty, Long> {
+}
---
+++ src/main/java/com/pblgllgs/sbpetclinic/service/map/SpecialityMapService.java
@@ -0,0 +1,35 @@
+package com.pblgllgs.sbpetclinic.service.map;
+
+import com.pblgllgs.sbpetclinic.model.Specialty;
+import com.pblgllgs.sbpetclinic.service.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Specialty,Long> implements SpecialityService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
